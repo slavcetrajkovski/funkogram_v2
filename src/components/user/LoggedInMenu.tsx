fe02046a95084856897d4191fb3a2c66e1c06b45
Slavcho <s.trajkovski02@yahoo.com>
2024-08-21 20:56:01 +0200

    small ui update

--- src/components/user/LoggedInMenu.tsx
+++ src/components/user/LoggedInMenu.tsx
@@ -4,13 +4,13 @@ export default function LoggedInMenu({ firstName }: { firstName: string }) {
 
     const handleLogout = () => {
         localStorage.removeItem("token");
-        
+
         window.location.reload();
     };
 
     return (
         <div className="relative">
-            <div className="absolute -right-24 mt-4 py-2 px-4 w-48 bg-white rounded-lg shadow-xl z-20">
+            <div className="absolute -right-16 mt-4 py-2 px-4 w-48 bg-white rounded-lg shadow-xl z-20">
                 <p className="px-4 py-2 text-gray-700 font-bold">Добредојдовте, <span className="text-funkogram_red">{firstName}</span></p>
                 <a href="/user/account" className="block py-2 text-white bg-funkogram_red rounded-full text-center font-bold hover:bg-red-700 transition">
                     Види профил
