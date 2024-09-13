9403097b57ecc098f320c055c6dd9c987ca8f0e9
Slavcho <s.trajkovski02@yahoo.com>
2024-09-13 14:13:16 +0200

    fix bug

--- src/data/CarouselImages.ts
+++ src/data/CarouselImages.ts
@@ -7,7 +7,7 @@ export interface CarouselImages {
 
 export const funkoImagesSlider1: CarouselImages[] = [
     { src: '/jack-sparrow.png', width: 1800, height: 600, link: "/catalog?searchFilter=jack+sparrow&page=0"},
-    { src: '/3.png', width: 1800, height: 600, link: "/catalog?searchFilter=Teenage+Mutant+Ninja+Turtles&page=0&productStatus=NEW_ARRIVAL" },
+    { src: '/3.png', width: 1800, height: 600, link: "/catalog?searchFilter=Teenage+Mutant+Ninja+Turtles&page=0&productStatusFilter=NEW_ARRIVAL" },
     { src: '/house-of-the-dragon.png', width: 1800, height: 600, link: "/catalog?searchFilter=house+of+the+dragon&page=0"},
     { src: '/lotr.png', width: 1800, height: 600, link: "/catalog?searchFilter=lord+of+the+rings&page=0"},
     { src: '/2.png', width: 1800, height: 600, link: "/catalog?searchFilter=Funko+POP%21+Marvel%3A+Spider-Man+2&page=0"}
@@ -16,14 +16,14 @@ export const funkoImagesSlider1: CarouselImages[] = [
 export const funkoImagesSlider2: CarouselImages[] = [
     { src: '/spider-man-across-the-spider-verse.png', width: 1800, height: 600, link: "/catalog?searchFilter=Spider-Man+Across+the+Spider-Verse&page=0" },
     { src: '/wednesday.png', width: 1800, height: 600, link: "/catalog?searchFilter=wednesday&page=0" },
-    { src: '/4.png', width: 1800, height: 600, link: "/catalog?searchFilter=nba&page=0&productStatus=PREORDER"},
+    { src: '/4.png', width: 1800, height: 600, link: "/catalog?searchFilter=nba&page=0&productStatusFilter=PREORDER"},
     { src: '/1.png', width: 1800, height: 600, link: "/catalog?searchFilter=chainsaw+man&page=0&productStatus=Сите"  },
     { src: '/invinicble.png', width: 1800, height: 600, link: "/catalog?searchFilter=invincible&page=0"},
 ]
 
 export const funkoImagesResponsiveSlider1: CarouselImages[] = [
     { src: '/responsive/10.png',  width: 700, height: 1027, link: "/catalog?searchFilter=jack+sparrow&page=0"},
-    { src: '/responsive/4.png',  width: 700, height: 1027, link: "/catalog?searchFilter=Teenage+Mutant+Ninja+Turtles&page=0&productStatus=NEW_ARRIVAL" },
+    { src: '/responsive/4.png',  width: 700, height: 1027, link: "/catalog?searchFilter=Teenage+Mutant+Ninja+Turtles&page=0&productStatusFilter=NEW_ARRIVAL" },
     { src: '/responsive/2.png',  width: 700, height: 1027, link: "/catalog?searchFilter=house+of+the+dragon&page=0"},
     { src: '/responsive/6.png',  width: 700, height: 1027, link: "/catalog?searchFilter=lord+of+the+rings&page=0"},
     { src: '/responsive/3.png',  width: 700, height: 1027, link: "/catalog?searchFilter=Funko+POP%21+Marvel%3A+Spider-Man+2&page=0"}
@@ -32,7 +32,7 @@ export const funkoImagesResponsiveSlider1: CarouselImages[] = [
 export const funkoImagesResponsiveSlider2: CarouselImages[] = [
     { src: '/responsive/5.png', width: 700, height: 1027, link: "/catalog?searchFilter=Spider-Man+Across+the+Spider-Verse&page=0" },
     { src: '/responsive/8.png',  width: 700, height: 1027, link: "/catalog?searchFilter=wednesday&page=0" },
-    { src: '/responsive/7.png',  width: 700, height: 1027, link: "/catalog?searchFilter=nba&page=0&productStatus=PREORDER"},
+    { src: '/responsive/7.png',  width: 700, height: 1027, link: "/catalog?searchFilter=nba&page=0&productStatusFilter=PREORDER"},
     { src: '/responsive/1.png',  width: 700, height: 1027, link: "/catalog?searchFilter=chainsaw+man&page=0&productStatus=Сите"  },
     { src: '/responsive/9.png', width: 700, height: 1027, link: "/catalog?searchFilter=invincible&page=0"},
 ]
