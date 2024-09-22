6fcf6a8cfc9a53ac87280c5afebb716920d268b5
Slavcho <s.trajkovski02@yahoo.com>
2024-09-22 14:20:42 +0200

    remove maintenance mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
 
-    const isMaintenancePage = true;
+    const isMaintenancePage = false;
 
     return (
         <html>
